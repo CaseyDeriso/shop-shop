3425ddfef1278e597d95f4d956806b8dcf06dcf4
Casey Deriso <caseyderiso@gmail.com>
2021-03-22 21:11:22 -0500

    wrote tests and reducers for updating category and setting current category

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -5,14 +5,25 @@ import {
 } from "./actions";
 
 export const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
-    case UPDATE_PRODUCTS: 
+    case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-    
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    // IF ACTION TYPE VALUE IS THE value of "UPDATE_CURRENT_CATEGORY", return an new state object with an updated current category
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
     // if none of these actions, do not update state
     default:
       return state;
