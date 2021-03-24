52ae6bb9f8b73f1efdf9ebd341f77768c6c85eca
Casey Deriso <caseyderiso@gmail.com>
2021-03-24 12:16:25 -0500

    converted detail.js to use global state object and removed useState from home.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,23 +2,51 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
+
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
+  // legacy code 
+  // const { id } = useParams();
+
+  // const [currentProduct, setCurrentProduct] = useState({})
+
+  // const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  // const products = data?.products || [];
+
+  // useEffect(() => {
+  //   if (products.length) {
+  //     setCurrentProduct(products.find(product => product._id === id));
+  //   }
+  // }, [products, id]);
+  ////////////////////////////////////////////////////////////////
+
+  const [state, dispatch] = useStoreContext();
+
   const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({})
+  const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  const { products } = data;
 
   useEffect(() => {
+    // if there are items in products to compare ids, run setCurrentProduct()
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
+    } else if (loading) {
+    // else, dispatch the global state from useQuery and trigger the effect again
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
     }
-  }, [products, id]);
+  }, [products, data, dispatch, id]);
 
   return (
     <>
